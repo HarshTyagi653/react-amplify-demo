2be0d9057620a2317210f246724bbd8cc21b827d
Rhythm Bhiwani <rhythm.devopspro@gmail.com>
2023-09-03 17:51:41 +0530

    React Project Added

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Blog from "./components/Blog";
+import CallToAction from "./components/CallToAction";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="space-y-40 mb-40">
+        <HeroSection />
+        <Features />
+        <Stats />
+        <Testimonials />
+        <CallToAction />
+        <Blog />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
